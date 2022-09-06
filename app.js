0bb6cb2b6e0359513509b354e8d1445878e8ddbe
grnonur <greenpikachuu@gmail.com>
2022-09-06 14:43:44 +0300

    view engine and db set up

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 import express from "express"
 import dotenv from "dotenv"
-
-const app = express();
+import mongoose from "mongoose"
 
 dotenv.config();
+const app = express();
+
+//database connection
+mongoose.connect(`mongodb://127.0.0.1/photopage`);
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 
 
 const port = process.env.PORT;
 
 
 app.get('/', (req, res) => {
-    res.send('ok!');
+    res.render('index');
 })
 
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
 app.listen(port, (req, res) => {
     console.log(`Server Aktif! http://127.0.0.1:${port}`);
